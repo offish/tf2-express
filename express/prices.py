751ee4a15736880723f415dbbec94e88182ed4d2
offish <overutilization@gmail.com>
2020-11-19 18:31:54 +0100

    Update prices.py

--- express/prices.py
+++ express/prices.py
@@ -1,61 +1,31 @@
-from .methods import request, read, write
-import json
+from .database import update_price, get_item
+from .database import _get_price
+from .methods import request
+from .logging import Log
 
-PATH = 'express/json/prices.json'
+
+log = Log()
 
 
 def get_pricelist():
     uri = 'https://api.prices.tf/items'
     payload = {'src': 'bptf'}
+    log.info('Trying to get prices...')
     return request(uri, payload)
 
 
-def get_price(item: str, intent: str, prices: dict):
-    if item in prices \
-        and intent in prices[item]:
-        return prices[item][intent]['metal']
-    return None
+def get_price(item: str, intent: str):
+    return _get_price(item)[intent]['metal']
 
 
-def get_prices(items: list, pricelist: dict) -> dict:
-    pricelist = pricelist['items'] \
-        if 'items' in pricelist else pricelist
-
-    prices = {}
-
-    for item in items:
-        for i in pricelist:
-            if i['name'] == item:
-                prices[i['name']] = {
-                    'buy': i['buy'],
-                    'sell': i['sell']
-                }
-    return prices
-
-
-def add(name: str, buy: float, sell: float) -> None:
-    data = read(PATH)
-
-    data[name] = {
-        'buy': buy,
-        'sell': sell
-    }
-    write(PATH, data)
-
-
-def update(name, buy, sell) -> None:
-    try:
-        data = read(PATH)
-        data[name]['buy'] = buy
-        data[name]['sell'] = sell
-        write(PATH, data)
-    except json.decoder.JSONDecodeError:
-        pass
-
-
-def remove(name: str) -> None:
-    data = read(PATH)
+def update_pricelist(items: list) -> None:
+    pricelist = get_pricelist()['items']
     
-    if name in data:
-        data.pop(name)
-        write(PATH, data)
+    for i in pricelist:
+        name = i['name']
+        if name in items:
+            item = get_item(name)
+
+            if not (i['buy'] == item['buy']) \
+                or not (i['sell'] == item['sell']):
+                update_price(name, i['buy'], i['sell'])
