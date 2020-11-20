1d7f010ffe3a6cc52361bb1186ae9c4bdf87df4f
offish <overutilization@gmail.com>
2020-11-20 19:55:58 +0100

    Update prices.py

--- express/prices.py
+++ express/prices.py
@@ -19,13 +19,24 @@ def get_price(item: str, intent: str):
 
 
 def update_pricelist(items: list) -> None:
-    pricelist = get_pricelist()['items']
-    
-    for i in pricelist:
+    pricelist = get_pricelist()
+
+    if not 'items' in pricelist:
+        log.error('Could not get pricelist')
+        return
+
+    for i in pricelist['items']:
         name = i['name']
+        
         if name in items:
             item = get_item(name)
 
-            if not (i['buy'] == item['buy']) \
+            if not (item['buy'] or item['sell']):
+                update_price(name, i['buy'], i['sell'])
+
+            elif not (i['buy'] == item['buy']) \
                 or not (i['sell'] == item['sell']):
                 update_price(name, i['buy'], i['sell'])
+
+    # Does not warn the user if an item in the database
+    # can't be found in Prices.TF's pricelist
