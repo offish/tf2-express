db0334c272e2c843681e33e71aef1660b85b5563
offish <overutilization@gmail.com>
2020-12-01 14:41:30 +0100

    Update prices.py

--- express/prices.py
+++ express/prices.py
@@ -2,9 +2,15 @@ from .database import update_price, get_item
 from .database import _get_price
 from .methods import request
 from .logging import Log
+from .utils import to_scrap, to_refined
 
+from .settings import craft_hat_buy, craft_hat_sell
 
 log = Log()
+
+
+def get_key_price() -> float:
+    return _get_price('Mann Co. Supply Crate Key')['buy']['metal']
 
 
 def get_pricelist() -> dict:
@@ -12,14 +18,25 @@ def get_pricelist() -> dict:
     return request('https://api.prices.tf/items', {'src': 'bptf'})
 
 
-def get_price(item: str, intent: str) -> float:
-    return _get_price(item)[intent]['metal']
+def get_price(name: str, intent: str) -> float:
+    if name == 'Random Craft Hat':
+        return craft_hat_sell if intent == 'sell' \
+            else craft_hat_buy
+
+    price = _get_price(name)[intent]
+    metal = to_scrap(price['metal'])
+    keys = price.get('keys')
+    
+    if keys:
+        metal += to_scrap(keys * get_key_price())
+    
+    return to_refined(metal)
 
 
 def update_pricelist(items: list) -> None:
     pricelist = get_pricelist()
 
-    if not 'items' in pricelist:
+    if not pricelist.get('items'):
         log.error('Could not get pricelist')
         return
 
