252fd88288a314efa4d892b43d8b315ed19b8344
offish <overutilization@gmail.com>
2021-01-12 16:30:53 +0100

    Update panel.py

--- express/ui/panel.py
+++ express/ui/panel.py
@@ -7,39 +7,36 @@ from .. import database
 
 app = Flask(__name__)
 
-running = True
 
-# Testing data
-@app.route('/')
-def index():
-    #data = {
-    #    'running': running,
-    #    'last_price_update': 456123146,
-    #    'items': len(database.get_items())
-    #}
-    #return render_template('index.html', data=data)
-    return redirect('/prices')
+@app.route("/")
+def overview():
+    return redirect("/prices")
 
 
-@app.route('/prices')
+@app.route("/trades")
+def trades():
+    return render_template("trades.html", trades=database.get_trades())
+
+
+@app.route("/prices")
 def prices():
-    return render_template('prices.html', items=database._get_pricelist())
+    return render_template("prices.html", items=database._get_pricelist())
 
 
-@app.route('/delete/<name>')
+@app.route("/delete/<name>")
 def delete(name):
     database.remove_price(name)
-    return redirect('/prices')
+    return redirect("/prices")
 
 
-@app.route('/add', methods=['POST'])
+@app.route("/add", methods=["POST"])
 def add():
     data = dict(request.form.items())
-    names = data['names'].split(', ')
+    names = data["names"].split(", ")
     for name in names:
         database.add_price(name)
-    return redirect('/prices')
+    return redirect("/prices")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
