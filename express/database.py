4137b5da2bdd01585cc4bb62235b3fbcc749a852
offish <overutilization@gmail.com>
2021-01-12 16:25:44 +0100

    Update database.py

--- express/database.py
+++ express/database.py
@@ -3,18 +3,18 @@ from .logging import Log
 from pymongo import MongoClient
 
 
-client = MongoClient('localhost', 27017)
-database = client['express']
+client = MongoClient("localhost", 27017)
+database = client["express"]
 
-trades = database['trades']
-prices = database['prices']
+trades = database["trades"]
+prices = database["prices"]
 
 log = Log()
 
 
 def add_trade(data: dict) -> None:
     trades.insert(data)
-    log.info(f'Offer was added to the database')
+    log.info(f"Offer was added to the database")
 
 
 def get_trades() -> dict:
@@ -22,15 +22,15 @@ def get_trades() -> dict:
 
 
 def get_items() -> list:
-    return [item['name'] for item in prices.find()]
+    return [item["name"] for item in prices.find()]
 
 
 def get_item(name: str) -> dict:
-    return prices.find_one({'name': name})
+    return prices.find_one({"name": name})
 
 
 def _get_price(name: str) -> dict:
-    return prices.find_one({'name': name})
+    return prices.find_one({"name": name})
 
 
 def _get_pricelist() -> dict:
@@ -38,23 +38,15 @@ def _get_pricelist() -> dict:
 
 
 def add_price(name: str) -> None:
-    prices.insert({
-        'name': name,
-        'buy': None,
-        'sell': None
-    })
-    log.info(f'Added {name} to the database')
+    prices.insert({"name": name, "buy": None, "sell": None})
+    log.info(f"Added {name} to the database")
 
 
 def update_price(name: str, buy: dict, sell: dict) -> None:
-    prices.replace_one({'name': name}, {
-        'name': name,
-        'buy': buy,
-        'sell': sell
-    })
-    log.info(f'Updated price for {name}')
+    prices.replace_one({"name": name}, {"name": name, "buy": buy, "sell": sell})
+    log.info(f"Updated price for {name}")
 
 
 def remove_price(name: str) -> None:
-    prices.delete_one({'name': name})
-    log.info(f'Removed {name} from the database')
+    prices.delete_one({"name": name})
+    log.info(f"Removed {name} from the database")
