ad7a1fc0f1b438d1ea58bbf47a4b0d1f13c1b57a
offish <overutilization@gmail.com>
2021-01-12 16:24:09 +0100

    Update __init__.py

--- express/__init__.py
+++ express/__init__.py
@@ -1,6 +1,4 @@
-
-__title__  = 'tf2-express'
-__author__ = 'offish'
-__license__ = 'MIT'
-__version__ = '1.2.5'
-
+__title__ = "tf2-express"
+__author__ = "offish"
+__license__ = "MIT"
+__version__ = "1.2.6"
