64c8cf107b22ed998b1b4a2d474ee9548b83ae52
offish <overutilization@gmail.com>
2021-05-31 20:33:28 +0200

    add autoprice key and rename def

--- express/database.py
+++ express/database.py
@@ -33,17 +33,19 @@ def _get_price(name: str) -> dict:
     return prices.find_one({"name": name})
 
 
-def _get_pricelist() -> dict:
+def get_database_pricelist() -> dict:
     return prices.find()
 
 
 def add_price(name: str) -> None:
-    prices.insert({"name": name, "buy": None, "sell": None})
+    prices.insert({"name": name, "autoprice": True, "buy": None, "sell": None})
     log.info(f"Added {name} to the database")
 
 
-def update_price(name: str, buy: dict, sell: dict) -> None:
-    prices.replace_one({"name": name}, {"name": name, "buy": buy, "sell": sell})
+def update_price(name: str, autoprice: bool, buy: dict, sell: dict) -> None:
+    prices.replace_one(
+        {"name": name}, {"name": name, "autoprice": autoprice, "buy": buy, "sell": sell}
+    )
     log.info(f"Updated price for {name}")
 
 
