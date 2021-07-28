196f4b362655ca348b46d7fb881e1192428bbbb9
offish <overutilization@gmail.com>
2021-07-28 18:17:12 +0200

    Update logging.py

--- express/logging.py
+++ express/logging.py
@@ -1,18 +1,41 @@
-from datetime import datetime
+from datetime import date, datetime
+
+from .config import DEBUG
 
 from colorama import Fore as f
 from colorama import init
 
 init()
 
+logs = []
+
+
+def write_to_txt_file(strings: list):
+    to_write = ""
+
+    for string in strings:
+        to_write += "\n" + string
+
+    with open("express/files/logs.txt", "a") as f:
+        f.write(to_write)
+
 
 def log(color: int, sort: str, bot: str, text: str, offer_id: str = ""):
-    name = f" {f.GREEN}[{bot}]" if bot else ""
+    name = f" {f.WHITE}[{f.GREEN}{bot}{f.WHITE}]" if bot else ""
     text = f"({f.YELLOW}{offer_id}{f.WHITE}) {text}" if offer_id else text
     time = datetime.now().time().strftime("%H:%M:%S")
-    print(
-        f"{f.GREEN}tf2-express | {f.WHITE}{time} - {color + sort}{name}{f.WHITE}: {text}{f.WHITE}"
-    )
+    string = "{}tf2-express | {}" + time + " - {}" + sort + "{}{}: " + text + "{}"
+
+    if DEBUG:
+        logs.append(
+            f"{date.today().strftime('%d/%m/%Y')} @ {time} | {sort} {bot}: {text}"
+        )
+
+        if len(logs) >= 10:
+            write_to_txt_file(logs)
+            logs.clear()
+
+    print(string.format(f.GREEN, f.WHITE, color, name, f.WHITE, f.WHITE))
 
 
 class Log:
@@ -28,3 +51,9 @@ class Log:
 
     def trade(self, text: str, offer_id: str = ""):
         log(f.MAGENTA, "trade", self.bot, text, self.offer_id or offer_id)
+
+    def debug(self, text: str, offer_id: str = ""):
+        log(f.CYAN, "debug", self.bot, text, self.offer_id or offer_id)
+
+    def close(self):
+        write_to_txt_file(logs)
